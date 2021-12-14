b3171bc3f12b3f2c949eddc3b973702fba45f664
Rustem A Dobrynia <hukutu4@users.noreply.github.com>
2021-12-15 01:44:14 +0300

    Update monitor-aleo.sh

--- monitor-aleo.sh
+++ monitor-aleo.sh
@@ -9,17 +9,25 @@ nodeState=$(curl -s --data-binary '{"jsonrpc": "2.0", "id":"documentation", "met
 minerAddress=$(journalctl -u aleod-miner.service --since="10 min ago" -o cat | grep confirmed_blocks | tail -n1 | awk '{print $14}' | sed -r 's/\)//g')
 blocksMined=$(journalctl -u aleod-miner.service --since="10 min ago" -o cat | grep confirmed_blocks | tail -n1 | awk '{print ($8+$11)}')
 
-latest_block_height=$(echo $nodeState | jq '.latest_block_height')
-latest_cumulative_weight=$(echo $nodeState | jq '.latest_cumulative_weight')
-number_of_candidate_peers=$(echo $nodeState | jq '.number_of_candidate_peers')
-number_of_connected_peers=$(echo $nodeState | jq '.number_of_connected_peers')
-number_of_connected_sync_nodes=$(echo $nodeState | jq '.number_of_connected_sync_nodes')
-software=$(echo $nodeState | jq '.software')
-status=$(echo $nodeState | jq '.status')
-type=$(echo $nodeState | jq '.type')
-version=$(echo $nodeState | jq '.version')
+latest_block_height=$(echo "$nodeState" | jq '.latest_block_height')
+latest_cumulative_weight=$(echo "$nodeState" | jq '.latest_cumulative_weight')
+number_of_candidate_peers=$(echo "$nodeState" | jq '.number_of_candidate_peers')
+number_of_connected_peers=$(echo "$nodeState" | jq '.number_of_connected_peers')
+number_of_connected_sync_nodes=$(echo "$nodeState" | jq '.number_of_connected_sync_nodes')
+software=$(echo "$nodeState" | jq '.software')
+status=$(echo "$nodeState" | jq '.status')
+type=$(echo "$nodeState" | jq '.type')
+version=$(echo "$nodeState" | jq '.version')
 
-logentry="blocksMined=$blocksMined"
+case $(echo "$status" | sed -r 's/\"//g') in
+"Mining") status_code=0;;
+"Ready") status_code=1;;
+"Peering") status_code=2;;
+"Syncing") status_code=3;;
+*) status_code=4;;
+esac
+
+logentry="blocksMined=$blocksMined,status_code=$status_code"
 logentry="$logentry,latest_block_height=$latest_block_height,latest_cumulative_weight=$latest_cumulative_weight,number_of_candidate_peers=$number_of_candidate_peers,number_of_connected_peers=$number_of_connected_peers,number_of_connected_sync_nodes=$number_of_connected_sync_nodes,software=$software,status=$status,type=$type,version=$version"
 logentry="$monitorName,minerAddress=$minerAddress $logentry $now"
 echo "$logentry"
